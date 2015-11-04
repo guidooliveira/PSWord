c7c86421afbb0c692578eb8e84b0aa72589d1a8c
Guido Oliveira <guido.oliveira@live.com>
2015-11-04 00:38:00 -0200

    New CmdLets added
    Add-WordText and Replace-WordText Added to the module

---
+++ PSWord/AddWordText.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using Novacode;
+using System.IO;
+using System.Diagnostics;
+using System.Drawing;
+
+
+namespace PSWord
+{
+    [Cmdlet(VerbsCommon.Add, "WordText")]
+    public class AddWordText : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory =true)]
+        public string FilePath { get; set; }
+        
+        [Parameter(Position = 1, ValueFromPipeline =true)]
+        public String[] Text { get; set; }
+        
+        [Parameter(HelpMessage = "Please choose a font size between 4 and 72")]
+        [ValidateRange(4, 72)]
+        public Int32 Size { get; set; }
+
+        [Parameter]
+        public SwitchParameter Bold { get; set; }
+
+        [Parameter]
+        public SwitchParameter Italic { get; set; }
+
+        [Parameter]
+        public SwitchParameter Show { get; set; }
+
+        [Parameter]
+        public FontFamily FontFamily { get; set; }
+
+        [Parameter]
+        public KnownColor FontColor { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            var fullPath = Path.GetFullPath(this.FilePath);
+            if (!File.Exists(fullPath))
+            {
+                var createDoc = DocX.Create(fullPath);
+                createDoc.Save();
+                createDoc.Dispose();
+            }
+        }
+
+        protected override void ProcessRecord()
+        {
+            ProviderInfo providerInfo = null;
+            var resolvedFile = this.GetResolvedProviderPathFromPSPath(this.FilePath, out providerInfo);
+            WriteVerbose(String.Format("Loading file {0}",resolvedFile[0]));
+
+            using (DocX document = DocX.Load(resolvedFile[0]))
+            {
+                var formatting = new Formatting
+                                     {
+                                        FontFamily = this.FontFamily,
+                                        Size = this.Size
+                                     };
+
+
+                if (this.Bold.IsPresent)
+                {
+                    formatting.Bold = true;
+                }
+                if (this.Italic.IsPresent)
+                {
+                    formatting.Italic = true;
+                }
+                formatting.FontColor = Color.FromKnownColor(this.FontColor);
+                
+                try
+                {
+                    foreach (var word in Text)
+                    {
+                        Paragraph p = document.InsertParagraph(word, false, formatting);
+                    }
+                        
+                    document.Save();
+                }
+                catch
+                {
+                }
+                finally
+                {
+                    if (this.Show.IsPresent)
+                    {
+                        Process.Start(resolvedFile[0]);
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ PSWord/ReplaceWordText.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using Novacode;
+using System.IO;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace PSWord
+{
+    
+
+    [Cmdlet("Replace", "WordText")]
+    public class ReplaceWordText : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string FilePath { get; set; }
+
+        [Parameter(Position = 1, ValueFromPipeline = true)]
+        public String NewText { get; set; }
+        
+        [Parameter(Position = 2)]
+        public String ReplacingText { get; set; }
+
+        [Parameter]
+        public SwitchParameter TrackChanges { get; set; }
+
+        [Parameter]
+        public SwitchParameter Show { get; set; }
+        protected override void BeginProcessing()
+        {
+            var fullPath = Path.GetFullPath(this.FilePath);
+            if (!File.Exists(fullPath))
+            {
+                var createDoc = DocX.Create(fullPath);
+                createDoc.Save();
+                createDoc.Dispose();
+            }
+        }
+
+        protected override void ProcessRecord()
+        {
+            ProviderInfo providerInfo = null;
+            var resolvedFile = this.GetResolvedProviderPathFromPSPath(this.FilePath, out providerInfo);
+            WriteVerbose(String.Format("Loading file {0}", resolvedFile[0]));
+
+            using (DocX document = DocX.Load(resolvedFile[0]))
+            {
+                
+
+                try
+                {
+                    if (this.TrackChanges.IsPresent)
+                    {
+                        document.ReplaceText(this.ReplacingText, this.NewText, true, RegexOptions.IgnoreCase);
+                    }
+                    else
+                    {
+                        document.ReplaceText(this.ReplacingText, this.NewText, false, RegexOptions.IgnoreCase);
+                    }
+
+                    document.Save();
+                }
+                catch (Exception exception)
+                {
+                    WriteError(new ErrorRecord(exception, exception.HResult.ToString(), ErrorCategory.WriteError, exception));
+                }
+                finally
+                {
+                    if (this.Show.IsPresent)
+                    {
+                        Process.Start(resolvedFile[0]);
+                    }
+                }
+            }
+        }
+    }
+}
+
