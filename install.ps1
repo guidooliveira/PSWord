a1e5a32ad632ceeabcae90bbf0825bbb03af98d4
Guido Oliveira <guido.oliveira@live.com>
2015-11-16 23:06:03 -0200

    install script

---
+++ install.ps1
@@ -0,0 +1,29 @@
+﻿$fileList = @(
+'DocX.dll',
+'MadMilkman.Docx.dll',
+'MadMilkman.Docx.xml',
+'PSWord.dll',
+'PSWord.psd1',
+'en-US/PSWord.dll-Help.xml'
+)
+
+
+$InstallDirectory = Join-Path -Path "$([Environment]::GetFolderPath('MyDocuments'))\WindowsPowershell\Modules" -ChildPath PSWord
+
+if (!(Test-Path $InstallDirectory))
+{
+    $null = mkdir $InstallDirectory
+    $null = mkdir $InstallDirectory\en-US
+}
+
+$wc = new-object System.Net.WebClient
+$fileList | ForEach-Object {
+    Try
+    {
+        $wc.DownloadFile("https://github.com/guidooliveira/PSWord/raw/master/Release/$_", "$installDirectory\$_") 
+    }
+    catch
+    {
+        Write-Error -Message "Error Downloading $_"
+    }
+}
