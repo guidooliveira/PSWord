a3d68c482abe2367a9cb15e2bf3f1a6695b446c8
Guido Oliveira <guido.oliveira@live.com>
2016-06-16 15:08:39 -0300

    Added NoNewLine and Heading Parameters to Add-WordText

--- PSWord/AddWordText.cs
+++ PSWord/AddWordText.cs
@@ -14,7 +14,7 @@ namespace PSWord
 {
     using System.Runtime.CompilerServices;
 
-    [Cmdlet(VerbsCommon.Add, "WordText")]
+    [Cmdlet(VerbsCommon.Add, "WordText", DefaultParameterSetName = "Default")]
     public class AddWordText : PSCmdlet
     {
         [Parameter(Position = 0, Mandatory =true)]
@@ -42,8 +42,14 @@ namespace PSWord
         [Parameter]
         public KnownColor FontColor { get; set; }
 
+        [Parameter(ParameterSetName = "Heading")]
+        public HeadingType Heading { get; set; }
+
+        [Parameter]
+        public SwitchParameter NoNewLine { get; set;}
         private DocX wordDocument { get; set; }
         private string resolvedPath { get; set; }
+        private Formatting formatting { get; set; }
         protected override void BeginProcessing()
         {
             this.resolvedPath = this.GetUnresolvedProviderPathFromPSPath(this.FilePath);
@@ -60,38 +66,84 @@ namespace PSWord
 
         protected override void ProcessRecord()
         {
-            var formatting = new Formatting
+            if (!(ParameterSetName == "Heading"))
             {
-                FontFamily = this.FontFamily,
-                Size = this.Size
-            };
+                formatting = new Formatting
+                {
+                    FontFamily = this.FontFamily,
+                    Size = this.Size
+                };
 
-            
+                if (this.Bold.IsPresent)
+                {
+                    formatting.Bold = true;
+                }
+                if (this.Italic.IsPresent)
+                {
+                    formatting.Italic = true;
+                }
 
-            if (this.Bold.IsPresent)
-            {
-                formatting.Bold = true;
+                formatting.FontColor = Color.FromKnownColor(this.FontColor);
             }
-            if (this.Italic.IsPresent)
-            {
-                formatting.Italic = true;
-            }
-
-            formatting.FontColor = Color.FromKnownColor(this.FontColor);
-
-
             if (this.Text.Length > 0)
             {
                 for (int i = 0; i < this.Text.Length; i++)
                 {
-                    Paragraph p = this.wordDocument.InsertParagraph(this.Text[i], false, formatting);
+                    if (this.NoNewLine.IsPresent)
+                    {
+                        if (this.Bold.IsPresent)
+                        {
+                            if (this.Italic.IsPresent)
+                            {
+                                var p = this.wordDocument.Paragraphs[this.wordDocument.Paragraphs.Count - 1].Append(this.Text[i]).Bold().Italic();
+                            }
+                            else
+                            {
+                                var p = this.wordDocument.Paragraphs[this.wordDocument.Paragraphs.Count - 1].Append(this.Text[i]).Bold();
+                            }
+                        }
+                        else
+                        {
+                            var p = this.wordDocument.Paragraphs[this.wordDocument.Paragraphs.Count - 1].Append(this.Text[i]);
+                        }
+                        if (this.Italic.IsPresent)
+                        {
+                            if (this.Bold.IsPresent)
+                            {
+                                var p = this.wordDocument.Paragraphs[this.wordDocument.Paragraphs.Count - 1].Append(this.Text[i]).Bold().Italic();
+                            }
+                            else
+                            {
+                                var p = this.wordDocument.Paragraphs[this.wordDocument.Paragraphs.Count - 1].Append(this.Text[i]).Italic();
+                            }
+                        }
+                        else
+                        {
+                            var p = this.wordDocument.Paragraphs[this.wordDocument.Paragraphs.Count - 1].Append(this.Text[i]);
+                        }
+
+                    }
+                
+            else
+            {
+                if (ParameterSetName == "Heading")
+                {
+                    var p = this.wordDocument.InsertParagraph(this.Text[i]);
+                    p.StyleName = this.Heading.ToString();
+                }
+                else
+                {
+                    var p = this.wordDocument.InsertParagraph(this.Text[i], false, formatting);
+                }
+
+            }
                 }
             }
             else
             {
                 try
                 {
-                    Paragraph p = this.wordDocument.InsertParagraph(this.Text[0], false, formatting);
+                    var p = this.wordDocument.InsertParagraph(this.Text[0], false, formatting);
                 }
                 catch (Exception exception)
                 {
